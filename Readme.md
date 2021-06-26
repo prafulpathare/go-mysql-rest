3398a61802734ccd1a7b0cf246398cdcaf6e2ba8
prafulpathare <patharepraful@gmail.com>
2021-06-26 22:02:41 +0530

    initiated base APIs

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# CRUD REST with GoLang
+
+
+
+#### References 
+* https://medium.com/@hugo.bjarred/rest-api-with-golang-mux-mysql-c5915347fa5b
---
+++ main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	_"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type Post struct {
+	ID          int32  `json:"id"`
+	UserId      int32  `json:"user_id"`
+	PostTitle   string `json:"title"`
+	PostText    string `json:"post_text"`
+	DateAndTime string `json:"dnt"`
+}
+
+type User struct {
+	ID      int32  `json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Address string `json:"address"`
+	Status  int16  `json:"status"`
+}
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/go_test")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	router := mux.NewRouter()
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", createPost).Methods("POST")
+	router.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+
+	router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/user/posts/{id}", getUserPosts).Methods("GET")
+	http.ListenAndServe(":8000", router)
+}
+
+// get all posts
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	m := make(map[string]interface{})
+	var posts []Post
+	result, err := db.Query("SELECT id, post_text, title, dnt, user_id from posts limit ")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+		var post Post
+		err := result.Scan(&post.ID, &post.PostText, &post.PostTitle, &post.DateAndTime, &post.UserId)
+		if err != nil {
+			panic(err.Error())
+		}
+		posts = append(posts, post)
+	}
+
+	m["data"] = &posts
+	json.NewEncoder(w).Encode(m)
+}
+func createPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	stmt, err := db.Prepare("INSERT INTO posts(title) VALUES(?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	postText := keyVal["post_text"]
+	_, err = stmt.Exec(postText)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "New post was created")
+}
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, post_text, user_id, dnt FROM posts WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var post Post
+	for result.Next() {
+		err := result.Scan(&post.ID, &post.PostText, &post.UserId, &post.DateAndTime)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(post)
+}
+
+// get posts from user ID
+func getUserPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var posts []Post
+	result, err := db.Query("SELECT id, post_text, dnt FROM posts WHERE user_id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	for result.Next() {
+		var post Post
+		err := result.Scan(&post.ID, &post.PostText, &post.DateAndTime)
+		if err != nil {
+			panic(err.Error())
+		}
+		posts = append(posts, post)
+	}
+	json.NewEncoder(w).Encode(posts)
+}
+
+// update post from ID
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE posts SET title = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newPostText := keyVal["post_text"]
+	_, err = stmt.Exec(newPostText, params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "Post with ID = %s was updated", params["id"])
+}
+
+// delete post from id
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("DELETE FROM posts WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Fprintf(w, "Post with ID = %s was deleted", params["id"])
+}
+
+// get user from ID
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, email, name, address, status FROM users WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+	var user User
+	for result.Next() {
+		err := result.Scan(&user.ID, &user.Email, &user.Name, &user.Address, &user.Status)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	json.NewEncoder(w).Encode(user)
+}
